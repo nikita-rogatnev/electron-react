8f2d325c39be2f2f06d26cb1826ce7ae13313e51
Rogatnev Nikita <nikita.rogatnev@ix.co>
2019-06-29 11:28:59 +0300

    add router

--- app/src/components/app/app.jsx
+++ app/src/components/app/app.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
+import {HashRouter, Route, Link} from 'react-router-dom';
+
+import Intro from '../intro/intro'
+import About from '../about/about'
 
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <div className="hello">
-                    <h1>Hell22o React!</h1>
-                </div>
-                <p>
-                    Electron app using React.
-                </p>
-            </div>
+            <HashRouter>
+                <nav>
+                    <Link to="/">Home</Link>
+                    <Link to="/about/">About</Link>
+                </nav>
+
+                <Route path="/" exact component={Intro}/>
+                <Route path="/about/" component={About}/>
+            </HashRouter>
         )
     }
 }
---
+++ app/src/components/intro/intro.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Intro extends React.Component {
+    render() {
+        return (
+            <h2>Intro</h2>
+        )
+    }
+}
+
+export default Intro;
